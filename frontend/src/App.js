5c8b76f09745ee1918bf7347811c3c23ffe01cf0
Marina Savitzki <marinasavitzki@gmail.com>
2020-09-21 15:17:37 -0300

    atualizando os arquivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Header from  './Header';
+import './global.css'
+import Routes from './routes';
+
+function App(){
+
+/*    const [counter, setCounter] = useState(0);
+
+    function increment(){
+        setCounter(counter + 1);
+    }
+*/
+    return(
+       /* <div>
+            <Header>Contador: {counter}</Header>
+            <button onClick={increment}>Incrementar</button>
+        </div>*/
+        <Routes />
+    );
 }
 
 export default App;
